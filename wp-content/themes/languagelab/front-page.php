3961c90a2d92fe2e61b172a7e7fa1f46edeebbe0
metamn <bartus.csongor@gmail.com>
2011-07-28 15:30:23 +0300

    Pages are showing the sidebar too

--- wp-content/themes/languagelab/front-page.php
+++ wp-content/themes/languagelab/front-page.php
@@ -17,6 +17,8 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
+      <?php the_post(); ?>
+			<?php get_template_part( 'content', 'page' ); ?>
 			
 			</div><!-- #content -->
 		</div><!-- #primary -->
